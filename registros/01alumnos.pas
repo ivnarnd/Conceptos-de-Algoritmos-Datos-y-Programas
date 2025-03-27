eb2129c5c4236f2f927e4e2c13127bbbc32379b3
Ivan Aranda <iaranda264@gmail.com>
2025-03-26 21:34:37 -0300

    realizado ejercicio 1a

--- registros/01alumnos.pas
+++ registros/01alumnos.pas
@@ -12,23 +12,34 @@ program Registros;
  type
  str20 = string[20];
  alumno = record
- codigo : integer;
- nombre : str20;
- promedio : real;
+	codigo : integer;
+	nombre : str20;
+	promedio : real;
  end;
  procedure leer(var alu : alumno);
  begin
- writeln('Ingrese el código del alumno');
- read(alu.codigo);
- if (alu.codigo <> 0) then begin
- writeln('Ingrese el nombre del alumno'); read(alu.nombre);
- writeln('Ingrese el promedio del alumno'); read(alu.promedio);
- end;
+	writeln('Ingrese el código del alumno');
+	readln(alu.codigo);
+	if (alu.codigo <> 0) then
+		begin
+			writeln('Ingrese el nombre del alumno');
+			readln(alu.nombre);
+			writeln('Ingrese el promedio del alumno');
+			readln(alu.promedio);
+		end;
  end;
  { declaración de variables del programa principal }
  var
  a : alumno;
+ cantLeidos:integer;
  { cuerpo del programa principal }
  begin
-
+	cantLeidos:=0;
+	leer(a);
+	while(a.codigo<>0)do
+		begin
+			cantLeidos:=cantLeidos+1;
+			leer(a);
+		end;
+	writeln('La cantidad de Alumnos leida es : ',cantLeidos);
  end.
